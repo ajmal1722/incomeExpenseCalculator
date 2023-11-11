9d51e7754d5ad2561dbcd52af0532edd332924f0
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-11 18:31:21 +0530

    expense list added and input displayed as an array

--- sample.js
+++ sample.js
@@ -1,9 +1,35 @@
-function getIncome (){
+function addIncome (){
 
     const inputIncomeElement = document.querySelector('.js-input-income');
     let inputIncome = inputIncomeElement.value;
     
     console.log (inputIncome);
-
+    inputIncomeElement.value = ''
     document.querySelector ('.js-display-income').innerHTML = ` $ ${inputIncome}`;
+}
+
+const expenseList = [];
+
+renderExpense();
+
+function renderExpense (){
+    expenseHTML = '';
+    for (let i = 0; i < expenseList.length; i++) {
+        const expense = expenseList[i];
+        const html = `<div>${expenseList}</div>`
+        expenseHTML += html;
+    }
+
+    document.querySelector('.js-display-expense').innerHTML = expenseHTML;
+}
+function addExpense () {
+    const inputExpenseElement = document.querySelector('.js-input-expense');
+    const inputExpense = inputExpenseElement.value;
+    
+    expenseList.push (inputExpense);
+    inputExpenseElement.value = '';
+    // expenseList.push(expense);
+    // document.querySelector('.js-display-expense').innerHTML = `<p>${expenseList}</p>`;
+        // console.log (expenseList); 
+    renderExpense();
 }
