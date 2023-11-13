cc3ea02ae7a8f121dca59ac5171fc72a32a90ef2
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-13 11:28:38 +0530

    restarting

--- sample.js
+++ sample.js
@@ -1,35 +1,35 @@
-function addIncome (){
+// function addIncome (){
 
-    const inputIncomeElement = document.querySelector('.js-input-income');
-    let inputIncome = inputIncomeElement.value;
+//     const inputIncomeElement = document.querySelector('.js-input-income');
+//     let inputIncome = inputIncomeElement.value;
     
-    console.log (inputIncome);
-    inputIncomeElement.value = ''
-    document.querySelector ('.js-display-income').innerHTML = ` $ ${inputIncome}`;
-}
+//     console.log (inputIncome);
+//     inputIncomeElement.value = ''
+//     document.querySelector ('.js-display-income').innerHTML = ` $ ${inputIncome}`;
+// }
 
-const expenseList = [];
+// const expenseList = [];
 
-renderExpense();
+// renderExpense();
 
-function renderExpense (){
-    expenseHTML = '';
-    for (let i = 0; i < expenseList.length; i++) {
-        const expense = expenseList[i];
-        const html = `<div>${expenseList}</div>`
-        expenseHTML += html;
-    }
+// function renderExpense (){
+//     expenseHTML = '';
+//     for (let i = 0; i < expenseList.length; i++) {
+//         const expense = expenseList[i];
+//         const html = `<div>${expenseList}</div>`
+//         expenseHTML += html;
+//     }
 
-    document.querySelector('.js-display-expense').innerHTML = expenseHTML;
-}
-function addExpense () {
-    const inputExpenseElement = document.querySelector('.js-input-expense');
-    const inputExpense = inputExpenseElement.value;
+//     document.querySelector('.js-display-expense').innerHTML = expenseHTML;
+// }
+// function addExpense () {
+//     const inputExpenseElement = document.querySelector('.js-input-expense');
+//     const inputExpense = inputExpenseElement.value;
     
-    expenseList.push (inputExpense);
-    inputExpenseElement.value = '';
-    // expenseList.push(expense);
-    // document.querySelector('.js-display-expense').innerHTML = `<p>${expenseList}</p>`;
-        // console.log (expenseList); 
-    renderExpense();
-}
+//     expenseList.push (inputExpense);
+//     inputExpenseElement.value = '';
+//     // expenseList.push(expense);
+//     // document.querySelector('.js-display-expense').innerHTML = `<p>${expenseList}</p>`;
+//         // console.log (expenseList); 
+//     renderExpense();
+// }
