6af5f4e470ec3eb74aee7ce6e28e874be405bd78
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2023-11-14 10:13:49 +0530

    find the total expenses

--- sample.js
+++ sample.js
@@ -1,5 +1,6 @@
 // To display total income 
 let income = 0;
+let totalExpense = 0;
 function getIncome () {
     const inputIncomeElement = document.querySelector('.js-input-income');
     const inputIncome = parseFloat (inputIncomeElement.value);
@@ -12,6 +13,8 @@ function getIncome () {
     } else {
         alert ('Enter a proper Income amount');
     }
+    const result = balance(inputExpense)
+        console.log(result)
 }
 
 
@@ -23,6 +26,7 @@ function getExpense() {
     const inputCategoryElement = document.querySelector('.js-input-category');
     const inputCategory = inputCategoryElement.value;
 
+    let totalExpanse = 0;
     if (inputExpenseElement.value !== '' && inputCategoryElement.value !== '') {
         const tableBody = document.querySelector('.js-table');
         const newRow = tableBody.insertRow();
@@ -33,10 +37,18 @@ function getExpense() {
         cellCategory.innerHTML = inputCategory;
         cellExpense.innerHTML = inputExpense;
 
+        // to find the total balance
+        totalExpense = totalExpense + inputExpense;
+        console.log (totalExpense);
+
         // Clear input fields
         inputExpenseElement.value = '';
         inputCategoryElement.value = '';
-
-        console.log(inputCategory);
+        
+        
     }
 }
+
+
+
+
